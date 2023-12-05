b1f970f933c511257e38fc68611787be63bebe65
Christopher Angelo <angelo@angeloanan.xyz>
2023-12-06 03:48:17 +0800

    deps: remove deprecated packages

--- README.md
+++ README.md
@@ -25,6 +25,8 @@ pnpm install
 
 ## Pre-production Checklist
 
+Must do:
+
 - [ ] Whitelabel package.json with your project information (enable `private: true` if needed)
 - [ ] Lock dependencies version to major version
 - [ ] Replace `site` on [astro.config.mjs](./astro.config.mjs) with your site URL (sitemap)
@@ -33,3 +35,8 @@ pnpm install
   - [ ] Optimize your robots.txt entry to account for [Verified Bots](https://radar.cloudflare.com/traffic/verified-bots)
 - [ ] Setup CI/CD that'll automatically deploy the app on update
   - [ ] Docker: Double check for dependencies vs dev-dependencies requirements
+
+Should do:
+
+- [astro.config.mjs](./astro.config.mjs)
+  - [ ] Adjust Astro's [prefetching strategy](https://docs.astro.build/en/guides/prefetch/#prefetch-configuration) to your likings
--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 import node from '@astrojs/node'
-import prefetch from '@astrojs/prefetch'
 import solidJs from '@astrojs/solid-js'
 import partytown from '@astrojs/partytown'
 import sitemap from '@astrojs/sitemap'
@@ -13,17 +12,11 @@ import robots from 'astro-robots'
 // https://astro.build/config
 export default defineConfig({
   site: 'https://angeloanan.xyz',
-  integrations: [
-    solidJs(),
-    tailwind(),
-    prefetch({
-      selector: 'a',
-      throttle: 10
-    }),
-    partytown(),
-    sitemap(),
-    robots()
-  ],
+  prefetch: {
+    // defaultStrategy: 'viewport',
+    prefetchAll: true
+  },
+  integrations: [solidJs(), tailwind(), partytown(), sitemap(), robots()],
   output: 'server',
   adapter: node({
     mode: 'standalone'
