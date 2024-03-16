93cd54745e32890a465cb4a7fd46f43eeec171dc
Christopher Angelo <angelo@angeloanan.xyz>
2024-03-17 00:42:55 +0700

    feat: allow typings for environment variables

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="@astrojs/client" />
+
+interface ImportMetaEnv {
+  // Add your own env variables here. Example:
+  // readonly ADMIN_PASSWORD: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
